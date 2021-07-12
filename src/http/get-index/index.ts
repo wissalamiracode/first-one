e423f57241aec908ed2ef4dda9562cc3481d5e31
Wissal Amira <79423488+wissalamiracode@users.noreply.github.com>
2021-07-12 09:44:36 +0200

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -28,8 +28,8 @@ export async function handler (req: object) {
     statusCode: 200,
     headers: {
       'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-      'Access-Control-Allow-Origin':  '*'
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+      'Access-Control-Allow-Origin':  '*',
      ' Access-Control-Allow-Methods': 'POST,GET'
       
     },
