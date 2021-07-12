0e20e1e74169b35d1d607c44cda0697f1532d5d3
Wissal AMIRA <wissal.amira@um5.ac.ma>
2021-07-12 03:17:10 +0200

    hhhh

---
+++ public/index.js
@@ -0,0 +1,24 @@
+(async function main() {
+    // Get a reference to the element to display our data in
+    var form=document.getElementById('formul');
+    form.onsubmit = function(event) {
+    var button=document.getElementById('submit');
+    // Declare a message variable to be set later
+    let req
+    try {
+      // Attempt to fetch data from our API
+      let data = await (await fetch('/api')).json()
+      console.log(data)
+      req = data.req
+    } catch (err) {
+      // If there was an error catch it and display the message
+      console.log(err)
+      req = err.req
+    }
+    // If we created the display element and added the ID correctly
+    if (form) {
+      // Show the message
+      form.innerHTML = req
+    }
+};
+  }())
