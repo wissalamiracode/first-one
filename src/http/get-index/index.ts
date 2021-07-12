7ae181f63bdf17bb2156839f8ad802ac9136589f
Wissal Amira <79423488+wissalamiracode@users.noreply.github.com>
2021-07-12 02:40:34 +0200

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -30,7 +30,7 @@ export async function handler (req: object) {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body
+    body:JSON.stringify(req)
   }
 }
 
